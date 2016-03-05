3cb10a022fa1bfb0e8420f95c814866d3a9f68de
Naoki Endoh <naoki.endoh@gmail.com>
2016-03-05 13:19:52 +0900

    Add misawa cool phrases

--- plugins/misawa.js
+++ plugins/misawa.js
@@ -1,29 +1,25 @@
+var _ = require('lodash');
 var request = require('request');
 
-var misawa = function(bot, message, q) {
+var ERROR_MSG = 'へんじがないただのしかばねのようだ';
+
+var misawa = function(bot, message, keyword) {
     request.get('http://horesase.github.io/horesase-boys/meigens.json', function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            var meigens = JSON.parse(body);
-            console.log(meigens);
+            var phrases = JSON.parse(body);
+
+            bot.reply(message, _.sample(phrases).image);
+        } else {
+            bot.reply(message, ERROR_MSG);
         }
     });
-    // message.http('http://horesase.github.io/horesase-boys/meigens.json').get()(function(err, res, body) {
-    //     var meigens;
-    //     if (err) {
-    //         bot.reply(message, ERROR_MESSAGE);
-    //     } else {
-    //         meigens = JSON.parse(body);
-    //         bot.reply(message, misawaN(message, q, meigens));
-    //     }
-    // });
 };
 
 module.exports = function(controller) {
-    controller.hears(['!misawa( +(.*))?'], ['direct_message'], function(bot, message) {
+    controller.hears(['!misawa( +(.*))?'], ['direct_message', 'ambient'], function(bot, message) {
         var matches = message.text.match(/!misawa( +(.*))?/i);
-        var q = matches[2];
+        var keyword = matches[2];
 
-        misawa(bot, message, q);
-        // return misawa(message, q, 1);
+        misawa(bot, message, keyword);
     });
 };
