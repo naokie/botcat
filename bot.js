9f0c2f5d9e109d98b07dca4e7be02ce354a7f59d
Naoki Endoh <naoki.endoh@gmail.com>
2018-02-08 02:00:27 +0900

    :lipstick: lint javascript styles

--- bot.js
+++ bot.js
@@ -8,158 +8,184 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  console.log("Error: Specify token in environment");
+  process.exit(1);
 }
 
-var Botkit = require('botkit');
-var redis = require('botkit-storage-redis');
-var os = require('os');
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
-var url = require('url');
+var Botkit = require("botkit");
+var redis = require("botkit-storage-redis");
+var os = require("os");
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var url = require("url");
 
-var NODE_ENV = process.env.NODE_ENV || 'development';
-
-console.log(NODE_ENV);
-console.log(NODE_ENV === 'development');
+var NODE_ENV = process.env.NODE_ENV || "development";
 
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var redisStorage = redis({
-    namespace: 'botcat',
-    host: redisURL.hostname,
-    port: redisURL.port,
-    auth_pass: redisURL.auth.split(':')[1],
+  namespace: "botcat",
+  host: redisURL.hostname,
+  port: redisURL.port,
+  auth_pass: redisURL.auth.split(":")[1]
 });
 
 var controller = Botkit.slackbot({
-    storage: redisStorage,
-    debug: NODE_ENV === 'development',
+  storage: redisStorage,
+  debug: NODE_ENV === "development"
 });
 
-var bot = controller.spawn({
+var bot = controller
+  .spawn({
     token: process.env.token
-}).startRTM();
+  })
+  .startRTM();
 
-var pluginsPath = path.resolve(__dirname, 'plugins');
+var pluginsPath = path.resolve(__dirname, "plugins");
 fs.readdir(pluginsPath, function(err, list) {
-    for (var file of list) {
-        var pluginPath = path.resolve(pluginsPath, file);
-        require(pluginPath)(controller);
-    }
+  console.log(list);
+  list.forEach(function(file) {
+    var pluginPath = path.resolve(pluginsPath, file);
+    require(pluginPath)(controller);
+  });
 });
 
-require('./cron/wakeup')(bot);
+require("./cron/wakeup")(bot);
 
-
-controller.hears(['hello', 'hi', 'こんにちわ', 'おはよう'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
+controller.hears(
+  ["hello", "hi", "こんにちわ", "おはよう"],
+  "direct_message,direct_mention,mention",
+  function(bot, message) {
+    bot.api.reactions.add(
+      {
         timestamp: message.ts,
         channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
+        name: "robot_face"
+      },
+      function(err, res) {
         if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
+          bot.botkit.log("Failed to add emoji reaction :(", err);
         }
-    });
-
+      }
+    );
 
     controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
+      if (user && user.name) {
+        bot.reply(message, "Hello " + user.name + "!!");
+      } else {
+        bot.reply(message, "Hello.");
+      }
     });
-});
+  }
+);
 
-controller.hears(['call me (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(
+  ["call me (.*)"],
+  "direct_message,direct_mention,mention",
+  function(bot, message) {
     var matches = message.text.match(/call me (.*)/i);
     var name = matches[1];
     controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
+      if (!user) {
+        user = {
+          id: message.user
+        };
+      }
+      user.name = name;
+      controller.storage.users.save(user, function(err, id) {
+        bot.reply(
+          message,
+          "Got it. I will call you " + user.name + " from now on."
+        );
+      });
     });
-});
+  }
+);
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
+controller.hears(
+  ["what is my name", "who am i"],
+  "direct_message,direct_mention,mention",
+  function(bot, message) {
     controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.reply(message, 'I don\'t know yet!');
-        }
+      if (user && user.name) {
+        bot.reply(message, "Your name is " + user.name);
+      } else {
+        bot.reply(message, "I don't know yet!");
+      }
     });
-});
+  }
+);
 
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
+controller.hears(
+  ["shutdown"],
+  "direct_message,direct_mention,mention",
+  function(bot, message) {
     bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
+      convo.ask("Are you sure you want me to shutdown?", [
         {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
+          pattern: bot.utterances.yes,
+          callback: function(response, convo) {
+            convo.say("Bye!");
+            convo.next();
+            setTimeout(function() {
+              process.exit();
+            }, 3000);
+          }
+        },
+        {
+          pattern: bot.utterances.no,
+          default: true,
+          callback: function(response, convo) {
+            convo.say("*Phew!*");
+            convo.next();
+          }
         }
-        ]);
+      ]);
     });
-});
+  }
+);
 
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'direct_message,direct_mention,mention', function(bot, message) {
-
+controller.hears(
+  ["uptime", "identify yourself", "who are you", "what is your name"],
+  "direct_message,direct_mention,mention",
+  function(bot, message) {
     var hostname = os.hostname();
     var uptime = formatUptime(process.uptime());
 
-    bot.reply(message, ':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-});
+    bot.reply(
+      message,
+      ":robot_face: I am a bot named <@" +
+        bot.identity.name +
+        ">. I have been running for " +
+        uptime +
+        " on " +
+        hostname +
+        "."
+    );
+  }
+);
 
 function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
+  var unit = "second";
+  if (uptime > 60) {
+    uptime = uptime / 60;
+    unit = "minute";
+  }
+  if (uptime > 60) {
+    uptime = uptime / 60;
+    unit = "hour";
+  }
+  if (uptime != 1) {
+    unit = unit + "s";
+  }
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
+  uptime = uptime + " " + unit;
+  return uptime;
 }
 
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Ok, dyno is awake.');
-}).listen(process.env.PORT || 5000);
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Ok, dyno is awake.");
+  })
+  .listen(process.env.PORT || 5000);
--- plugins/dialogue.js
+++ plugins/dialogue.js
@@ -1,42 +1,52 @@
-var request = require('request');
+var request = require("request");
 
 module.exports = function(controller) {
-    controller.hears(['.+'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-        if (!process.env.DOCOMO_DIALOGUE_API_KEY) return;
+  controller.hears(
+    [".+"],
+    ["direct_message", "direct_mention", "mention"],
+    function(bot, message) {
+      if (!process.env.DOCOMO_DIALOGUE_API_KEY) return;
 
-        var p = parseFloat(process.env.DOCOMO_DIALOGUE_P || '0.3');
-        if (Math.random() < p) {
-            bot.reply(message, 'ニャン');
-            return;
+      var p = parseFloat(process.env.DOCOMO_DIALOGUE_P || "0.3");
+      if (Math.random() < p) {
+        bot.reply(message, "ニャン");
+        return;
+      }
+
+      var matches = message.text.match(/(.+)/i);
+
+      bot.api.users.info(
+        {
+          user: message.user
+        },
+        function(error, response) {
+          if (error) {
+            console.log(error);
+          } else {
+            var payload = {
+              utt: matches[0],
+              nickname: response.user.name
+            };
+
+            request.post(
+              {
+                url: "https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue",
+                qs: {
+                  APIKEY: process.env.DOCOMO_DIALOGUE_API_KEY
+                },
+                json: payload
+              },
+              function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  bot.reply(message, body.utt);
+                } else {
+                  console.log(error);
+                }
+              }
+            );
+          }
         }
-
-        var matches = message.text.match(/(.+)/i);
-
-        bot.api.users.info({
-            user: message.user
-        }, function(error, response) {
-            if (error) {
-                console.log(error);
-            } else {
-                var payload = {
-                    utt: matches[0],
-                    nickname: response.user.name
-                };
-
-                request.post({
-                    url: 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue',
-                    qs: {
-                        APIKEY: process.env.DOCOMO_DIALOGUE_API_KEY,
-                    },
-                    json: payload,
-                }, function(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        bot.reply(message, body.utt);
-                    } else {
-                        console.log(error);
-                    }
-                });
-            }
-        });
-    });
+      );
+    }
+  );
 };
